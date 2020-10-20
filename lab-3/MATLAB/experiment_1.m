12846d46688f9392573b32096bb97f0439c1c21e
Jasper Katzban <jasperkatzban@gmail.com>
2020-10-20 16:13:19 -0400

    plot given data for experiment 1

--- lab-3/MATLAB/experiment_1.m
+++ lab-3/MATLAB/experiment_1.m
@@ -1,10 +1,9 @@
 %% Lab 3: Resistors & Bipolar Transistors
+% Experiment 1: Bipolar Transistor Characteristics
 load('lab3exp1.mat');
 U_T = .025; %V
 
-%% Experiment 1: Bipolar Transistor Characteristics
-
-% Semilong plot showing the collector current AND base current as a function of base voltage. 
+%% Semilog plot of collector current AND base current as a function of base voltage. 
 % Should include appropriate theoretical fits (just use Curve Fit Tool I guess…) and extracted parameter values?
 
 % Plot given data
@@ -14,8 +13,8 @@ grid on;
 title('I_b, I_c and V_b with Theoretical fits');
 xlabel('V_{be} (V)'); ylabel('Current (A)');
 
-plot(Vbe, Ib,'.');
-plot(Vbe, Ic,'.');
+% plot(Vbe, Ib,'.');
+% plot(Vbe, Ic,'.');
 
 % Plot theoretical of I_c
 [xData, yData] = prepareCurveData( Vbe, Ic );
@@ -31,7 +30,12 @@ opts.Normalize = 'on';
 [fitresult, gof] = fit( xData, yData, ft, opts );
 
 % Plot fit with data.
-plot(fitresult, xData, yData );
+% plot(fitresult, xData, yData );
+    plot(xData, yData, '.');
+
+I_s_t = 3e-15;
+I_c_t = I_s_t.*exp(xData./U_T);
+plot(xData, I_c_t);
 
 % theoretical from I_b
 [xData2, yData2] = prepareCurveData( Vbe, Ib );
@@ -47,24 +51,33 @@ opts.StartPoint = [5.00918741733474e-06 4.84284559039923];
 [fitresult2, gof] = fit( xData2, yData2, ft, opts2 );
 
 % Plot fit with data.
-plot(fitresult2, xData2, yData2 );
+% plot(fitresult2, xData2, yData2);
+plot(xData2, yData2, '.');
+
+beta = Ic./Ib;
+
+I_b_t = I_c_t./beta';
+plot(xData2, I_b_t);
 
 % Extract fitted values
+coeffs = coeffvalues(fitresult);
 coeffs2 = coeffvalues(fitresult2);
 beta = Ic./Ib;
 
-U_T_fit2 = 1/coeffs2(2);
-I_S_fit2 = mean(coeffs2(1)./beta);
+U_T_f = 1/(10*coeffs(2));
+I_S_f = mean(coeffs2(1)./beta);
 
-str1 = sprintf('U_T = %f V', (U_T_fit));
-str2 = sprintf('I_S = %.4s A', (I_S_fit));
+str1 = sprintf('U_T = %.4f V', (U_T_f));
+str2 = sprintf('I_S = %.4s A', (I_S_f));
 % + newline + sprintf('I_S = %f', (I_S))
-annotation('textbox',[0.15, 0.6, 0.1, 0.1],'String',str1,'FitBoxToText','on', 'BackgroundColor', 'w');
-annotation('textbox',[0.15, 0.5, 0.1, 0.1],'String',str2,'FitBoxToText','on','BackgroundColor', 'w');
+annotation('textbox',[0.15, 0.8, 0.1, 0.1],'String',str1,'FitBoxToText','on', 'BackgroundColor', 'w');
+annotation('textbox',[0.15, 0.7, 0.1, 0.1],'String',str2,'FitBoxToText','on','BackgroundColor', 'w');
 
 set(gca, 'yscale',  'log');
-legend('I_b', 'I_c','I_b (fit)', 'I_c (fit)','Location','northwest');
+legend('I_b', 'I_b (fit)', 'I_c', 'I_c (fit)','Location','southeast');
 
+% The collector current follows an exponential relationship with the base
+% voltage
 
 %% Plot current gain (beta = Ic/Ib) as a function of the base current (FIG 2)
 % (make the Ib axis logarithmic).
@@ -77,21 +90,34 @@ xlabel('I_b (A)'); ylabel('\beta');
 
 beta = Ic./Ib;
 plot(Ib,beta);
-set(gca, 'yscale','log');
+set(gca, 'xscale','log');
 
-% Extract the incremental base resistance (rb) from the base current-voltage characteristic
+legend('\beta (measured)', 'Location', 'southeast');
+    
+% Question:
+% Current gain is not constant with base current across the entire
+% response, but it is reasonable to assume that it's 
+
+% If not, under what circumstances would it be reasonable to assume it to be constant?
+
+%% Extract the incremental base resistance (rb) from the base current-voltage characteristic
 % make a log-log plot (FIG 3) of rb vs. Ib WITH a theoretical fit.
 
 % Assume forward active since collector current is held at 5V
 figure(3);
 hold on;
 grid on;
-title('r_d as a function of I_b');
-xlabel('r_d'); ylabel('I_b (A)');
+title('r_b as a function of I_b');
+xlabel('I_b (A)'); ylabel('r_b');
 
-rd = U_T./Ib;
-plot(Ib, rd);
+rb_f = U_T_f./Ib;
+rb_t = U_T./Ib;
+
+plot(Ib, rb_f, '.'); F3L1 = 'r_b (measured)';
+plot(Ib, rb_t); F3L2 = 'r_b(theoretical)';
+
 set(gca, 'yscale','log');
+legend(F3L1, F3L2);
 
 % Theoretical fit
 
@@ -105,10 +131,84 @@ grid on;
 title('g_m as a function of I_c');
 xlabel('I_c (A)'); ylabel('g_m');
 
-gm = Ic/U_T;
+gm_f = Ic./U_T_f;
+gm_t = Ic./U_T;
 
-plot(Ic, gm);
+plot(Ic, gm_f, '.'); F4L1 = 'g_m (measured)';
+plot(Ic, gm_t); F4L2 = 'g_m (theoretical)';
 set(gca, 'yscale','log');
 set(gca, 'xscale','log');
 
-% Plot theoretical
+legend(F4L1, F4L2, 'Location', 'southeast');
+
+%% Theoretical Gummel Plot
+
+% Plot given data
+figure(6);
+hold on;
+grid on;
+title('I_b, I_c and V_b with Theoretical fits');
+xlabel('V_{be} (V)'); ylabel('Current (A)');
+
+% plot(Vbe, Ib,'.');
+% plot(Vbe, Ic,'.');
+
+% Plot theoretical of I_c
+[xData, yData] = prepareCurveData( Vbe, Ic );
+
+% Set up fittype and options.
+ft = fittype( 'exp1' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares');
+opts.Display = 'Off';
+opts.Normalize = 'on';
+% opts.StartPoint = [5.00918741733474e-06 4.84284559039923];
+
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+
+% Plot fit with data.
+% plot(fitresult, xData, yData );
+    plot(xData, yData, '.');
+
+I_s_t = 3e-15;
+I_c_t = I_s_t.*exp(xData./U_T);
+plot(xData, I_c_t);
+
+% theoretical from I_b
+[xData2, yData2] = prepareCurveData( Vbe, Ib );
+
+% Set up fittype and options.
+ft2 = fittype( 'exp1' );
+opts2 = fitoptions( 'Method', 'NonlinearLeastSquares');
+opts2.Display = 'Off';
+opts2.Normalize = 'on';
+opts.StartPoint = [5.00918741733474e-06 4.84284559039923];
+
+% Fit model to data.
+[fitresult2, gof] = fit( xData2, yData2, ft, opts2 );
+
+% Plot fit with data.
+% plot(fitresult2, xData2, yData2);
+plot(xData2, yData2, '.');
+
+beta = Ic./Ib;
+
+I_b_t = I_c_t./beta';
+plot(xData2, I_b_t);
+
+% Extract fitted values
+coeffs = coeffvalues(fitresult);
+coeffs2 = coeffvalues(fitresult2);
+beta = Ic./Ib;
+
+U_T_f = 1/(10*coeffs(2));
+I_S_f = mean(coeffs2(1)./beta);
+
+str1 = sprintf('U_T = %.4f V', (U_T_f));
+str2 = sprintf('I_S = %.4s A', (I_S_f));
+% + newline + sprintf('I_S = %f', (I_S))
+annotation('textbox',[0.15, 0.8, 0.1, 0.1],'String',str1,'FitBoxToText','on', 'BackgroundColor', 'w');
+annotation('textbox',[0.15, 0.7, 0.1, 0.1],'String',str2,'FitBoxToText','on','BackgroundColor', 'w');
+
+set(gca, 'yscale',  'log');
+legend('I_b', 'I_b (fit)', 'I_c', 'I_c (fit)','Location','southeast');
